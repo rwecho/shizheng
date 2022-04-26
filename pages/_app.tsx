0762968da440f5955000833d4a6801a3e6351194
rwecho <rwecho@live.com>
2022-04-26 09:43:44 +0800

    add 4.25 image

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-
+import Head from 'next/head'
+import { ChakraProvider } from '@chakra-ui/react'
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Wind Sword</title>
+      </Head>
+
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
+  )
 }
 
 export default MyApp
